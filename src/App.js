c3a4d9adfd81c45d16d2bc82d6cb42ba870545af
EshuShango <tobiakinyemi21@gmail.com>
2023-03-15 03:57:09 -0500

    Got abt Btn within the Nav

--- src/App.js
+++ src/App.js
@@ -14,39 +14,39 @@ import {
   theme,
 } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./components/ColorModeSwitcher";
-import { Header} from "./components/index";
+import { Header } from "./components/index";
+
 // eslint-disable-next-line no-unused-vars
 
 import "./styles/global.css";
 
 function App() {
-
-
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
-        <Header   />
-      {<abtMe /> }
-           
+        <Header />
+
         <Grid minH="100vh" p={3}>
           {/* <Header theme={<ColorModeSwitcher justifySelf="flex-end" />} /> */}
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-              <Link
-                color="teal.500"
-                href="https://linktr.ee/oluanuakin"
-                fontSize="2xl"
-                target="_blank"
-                rel="noopener noreferrer"
+          <ColorModeSwitcher justifySelf="flex-middle" />
+          <VStack spacing={10}>
+            <Link
+              color="teal.500"
+              href="https://linktr.ee/oluanuakin"
+              fontSize="2xl"
+              target="_blank"
+              rel="noopener noreferrer"
               >
-                øLu Tobi Akinyemi <br/>
-                (click)
-              </Link>
+              øLu Tobi Akinyemi <br />
+              (click)
+            </Link>
+            
           </VStack>
         </Grid>
       </Box>
     </ChakraProvider>
   );
+
 }
 
 export default App;
